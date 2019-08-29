9c814b6c3289bad50057e652c27670d0b0717f4e
Amy Mok <amy.mok@gsa.gov>
2019-08-29 09:46:12 -0700

    Use Web Server (#34)
    * Update dependencies, use a web server, add 2 instances
    
    * Add dev and prod deployment
    
    * Add CF CLI
    
    * Add staging

--- usda_fns_ingestor/run.sh
+++ usda_fns_ingestor/run.sh
@@ -1,5 +1,13 @@
-python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user1', 'user1@usda.gov', '$APP_PASSWORD')"
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user2', 'user1@usda.gov', '$APP_PASSWORD')"
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user3', 'user1@usda.gov', '$APP_PASSWORD')"
-python manage.py runserver 0.0.0.0:8080
+#!/bin/bash
+echo "------ Starting APP ------"
+if [ $CF_INSTANCE_INDEX = "0" ]; then
+    echo "----- Migrating Database -----"
+    python manage.py migrate --noinput
+    echo "----- Creating Users -----"
+    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user1', 'user1@usda.gov', '$APP_PASSWORD')"
+	python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user2', 'user1@usda.gov', '$APP_PASSWORD')"
+	python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('user3', 'user1@usda.gov', '$APP_PASSWORD')"
+fi
+
+python manage.py collectstatic --noinput
+gunicorn -t 120 -k gevent -w 2 usda_fns_ingestor.wsgi:application
