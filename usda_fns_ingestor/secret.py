ead7e6f0cc7b681030e2c3a2707587112b7fd245
Amy Mok <amy.mok@gsa.gov>
2019-09-25 19:29:29 -0700

    Fix flake8 issues

--- usda_fns_ingestor/secret.py
+++ usda_fns_ingestor/secret.py
@@ -2,6 +2,7 @@ import argparse
 import string
 import secrets
 
+
 def generate_secret(length):
     secret = ''
     choice = string.digits + string.ascii_letters + string.punctuation
@@ -12,8 +13,10 @@ def generate_secret(length):
 
     return secret
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Generate a secret key/password.  The string will include digits, letters, and punctuation, and has a default length of 50.")
+    parser = argparse.ArgumentParser("Generate a secret key/password.  The string will include digits, letters, "
+                                     "and punctuation, and has a default length of 50.")
     parser.add_argument("-l", "--length", help="Specify secret length", type=int, default=50)
 
     args = parser.parse_args()
