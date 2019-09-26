684de4043e1c0d0a98072bef726f43848cb6b9d2
Amy Mok <amy.mok@gsa.gov>
2019-09-25 23:15:29 -0700

    Remove commented out code

--- usda_fns_ingestor/usda_fns_ingestor/settings/base.py
+++ usda_fns_ingestor/usda_fns_ingestor/settings/base.py
@@ -16,8 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = env.get_credential('DJANGO_SECRET_KEY', get_random_string(50))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
