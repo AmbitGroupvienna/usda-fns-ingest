ed0a5e8c6869ce1472090e5df2d102cb61822078
Amy Mok <amy.mok@gsa.gov>
2019-10-05 01:24:19 -0700

    Update dependencies, fix logging config, change allow_host to *

--- usda_fns_ingestor/usda_fns_ingestor/settings/cloud.py
+++ usda_fns_ingestor/usda_fns_ingestor/settings/cloud.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from .base import *  # noqa
 from .env import env
 from secret import generate_random_string
@@ -9,6 +10,12 @@ SECRET_KEY = env.get_credential('APP_SECRET_KEY', generate_random_string(50))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# Get to the base directory above the current app directory.  In cloud.gov, HOME is /home/vcap/app.
+# Going one level above to /home/vcap
+BASE_DIR = Path(os.getenv("HOME"))
+LOGS_DIR = Path(BASE_DIR).joinpath('logs')
+LOGS_DIR.mkdir(exist_ok=True)
 
 LOGGING = {
     'version': 1,
@@ -24,25 +31,40 @@ LOGGING = {
         },
     },
     'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOGS_DIR, 'usda_fns_ingestor.log'),
+            'formatter': 'verbose',
+        },
+
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
+            'stream': 'ext://sys.stdout',
         },
     },
     'loggers': {
         '': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'handlers': ['console', 'file'],
+            'propagate': True,
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
         'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-        },
-        'django.template': {
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
             'propagate': True,
             'level': 'INFO',
         },
+        'django.template': {
+            'handlers': ['console', 'file'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'ReVAL': {
+            'handlers': ['console', 'file'],
+            'propagate': True,
+            'level': 'INFO',
+        }
     },
 }
